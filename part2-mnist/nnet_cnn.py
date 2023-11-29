3af391187d7e80bc7cb8cf1e953d61a0847e311c
VTsioni <87659035+VTsioni@users.noreply.github.com>
2023-11-29 16:48:51 +0100

    cnn implementation

--- part2-mnist/nnet_cnn.py
+++ part2-mnist/nnet_cnn.py
@@ -8,7 +8,7 @@ import torch.autograd as autograd
 import torch.nn.functional as F
 import torch.nn as nn
 import sys
-sys.path.append("..")
+sys.path.append("C:\\Users\\vasts\\Documents\\MIT\\MIT Statistics and Data Science\\mit686sup\\2\\resources_mnist\\mnist")
 import utils
 from utils import *
 from train_utils import batchify_data, run_epoch, train_model, Flatten
@@ -44,10 +44,17 @@ def main():
     #################################
     ## Model specification TODO
     model = nn.Sequential(
-              nn.Conv2d(1, 32, (3, 3)),
-              nn.ReLU(),
-              nn.MaxPool2d((2, 2)),
-            )
+          nn.Conv2d(1, 32, (3, 3)),
+          nn.ReLU(),
+          nn.MaxPool2d((2, 2)),
+          nn.Conv2d(32, 64, (3, 3)),
+          nn.ReLU(),
+          nn.MaxPool2d((2, 2)),
+          Flatten(),
+          nn.Linear(1600, 128),
+          nn.Dropout(0.5),
+          nn.Linear(128, 10),
+        )
     ##################################
 
     train_model(train_batches, dev_batches, model, nesterov=True)
